2b48344bdfbe05f2a8b437def58b39138bcadc11
Benjamin Neumann <btdn@lab-btdn.com>
2020-09-29 23:20:24 -0500

    Use chocochip reciepes from https://kixlab.github.io/recipescape-dashboard/trees/chocochip.

--- retrieve_recipes.py
+++ retrieve_recipes.py
@@ -1,64 +1,39 @@
-# recipe.hyeungshikjung.com/recipe/trees/potatosalad
-# ~/recipe/recipe/<id> : get recipe using id
-# ~/recipe/recipes/<dishname>: get all the recipes of dish (currently only ‘potatosalad’ is available)
-# ~/recipe/clusters/<dishname>: get all clustering result of dish
+# recipe/<id>: get recipe using id
+# recipes/<dishname>: get all the recipes of dish
+# clusters/<dishname>: get all clustering result of dish
 
 import json
-import requests
-import pprint as pp
 
-servername = "http://recipe.hyeungshikjung.com/"
+server = "http://recipe.hyeungshikjung.com/recipe/"
+datadir = "./example_recipes/"
 
-# dishname = "potatosalad"
-# recipeid = ""
+def get_json_from_file(query):
+    with open(datadir+query+".json") as json_file:
+        return json.load(json_file)
 
-def get_recipes_json(dishname):
-    #getting annotated text instructions for all recipes for dishname
-
+def get_json_from_server(query):
     try:
-        r = requests.get(servername+"recipe/recipes/"+dishname)
+        r = requests.get(servername+query)
         r.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
         sys.exit(1)
 
-    #recipes = r.json()
-    #pp.pprint(recipes) 
-    return r.json()
+def get_json(query):
+    return get_json_from_file(query)
+
+def get_recipes_json(dishname):
+    #getting annotated text instructions for all recipes for dishname
+    return get_json("recipes/"+dishname)
 
 
 def get_all_recipes(dishname):
     #getting annotated text instructions for all recipes for dishname
-
-    try:
-        r = requests.get(servername+"recipe/trees/"+dishname)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-        sys.exit(1)
-
-    #recipes = r.json()
-    #pp.pprint(recipes) 
-    return r.json()
+    return get_json("trees/"+dishname)
     
 def get_single_recipe(recipeid):
-    #getting a single recipe 
-    
-    try:
-        r = requests.get(servername+"recipe/recipe/"+recipeid)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-        sys.exit(1)
-
-    return r.json()
+    #getting a single recipe
+    return get_json("recipe/"+recipeid)
 
 def get_clusters(dishname):
-    try:
-        r = requests.get(servername+"recipe/clusters"+dishname)
-        r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-        sys.exit(1)
-
-    return r.json()
+    return get_json("clusters"+dishname)
